0b441f73d30263ad8a5f3045af4283fb4167c814
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-05-21 15:19:01 -0300

    caso de uso crear publiacion

---
+++ src/modules/posts/application/dtos/CreatePostDto.ts
@@ -0,0 +1,3 @@
+export class CreatePostDto {
+  constructor(public readonly title: string, public readonly content: string) {}
+}
---
+++ src/modules/posts/application/useCases/createPostUseCase.ts
@@ -0,0 +1,18 @@
+import { PostRepository } from "@posts/domain/repositories/PostRepository";
+import { CreatePostDto } from "@posts/application/dtos/createPostDto";
+import { Post } from "@posts/domain/entities/Post";
+import { randomUUID } from "crypto";
+
+export class CreatePostUseCase {
+  constructor(private readonly postRepository: PostRepository) {}
+
+  async execute(newPost: CreatePostDto): Promise<void> {
+    const post = new Post(
+      randomUUID(),
+      newPost.title,
+      newPost.content,
+      new Date()
+    );
+    await this.postRepository.save(post);
+  }
+}
