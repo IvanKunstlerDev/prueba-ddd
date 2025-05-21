906bd2c3d4f54d8eaf33845a16232a3e88aaa183
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-05-21 18:53:36 -0300

    validaciones en la entidad post

--- src/modules/posts/domain/entities/Post.ts
+++ src/modules/posts/domain/entities/Post.ts
@@ -1,8 +1,24 @@
+import { UUID } from "crypto";
+
 export class Post {
   constructor(
-    public readonly id: string,
+    public readonly id: UUID,
     public readonly title: string,
     public readonly content: string,
     public readonly pubDate: Date
-  ) {}
+  ) {
+    this.validate();
+  }
+
+  private validate() {
+    if (this.title.length < 5) {
+      throw new Error("El titulo debe tener al menos 5 caracteres.");
+    }
+    if (this.content.length < 10) {
+      throw new Error("El contenido debe tener al menos 10 caracteres.");
+    }
+    if (this.content.length > 255) {
+      throw new Error("El contenido debe tener menos de 255 caracteres.");
+    }
+  }
 }
